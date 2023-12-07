05556b63fe232deb47c43bdc5845b164bc4bcdf4
levent581 <levent581@BPC03>
2023-12-07 13:17:29 +0300

    Proje dosyası ekle.

---
+++ SiraSizde/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiraSizde
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EkranIslem ei = new EkranIslem();
+            ei.EkranaYaz(0,2);
+            Console.WriteLine("=======================");
+            ei.EkranaYaz();
+        }
+    }
+    class EkranIslem
+    {
+        public void EkranaYaz(params int[] sayilar)
+        {
+            if (sayilar.Length == 0)
+            {
+                Console.WriteLine("Parametre olmadığı için metottan çıkılıyor.");
+                return;
+            }
+            Console.WriteLine("Parametreden gelen değerler:");
+            foreach (var s in sayilar)
+            {
+                if (s == 0)
+                {
+                    return;
+                }
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
